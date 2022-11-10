8c4e3c4a96f5c3fcc275bbd24fa19fe64a87382a
KIM JUN HYEOK <kjh45388117@gmail.com>
2022-11-10 21:49:36 +0900

    react_inf

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+//*react 라이브러리에서 React와 react 컴포넌트라는 클래스도 가져오기*/
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  btnStyle = {
+    color: "#fff",
+    backgroundColor: "orange",
+    border: "none",
+    padding: "5px 9px",
+    vorderRadius: "50%",
+    cursor: "pointer",
+    float: "right",
+  };
+
+  getStyle = () => {
+    return {
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: "none",
+    };
+  };
+
+  todoData = [
+    {
+      id: "1",
+      title: "공부하기",
+      completed: true,
+    },
+    {
+      id: "2",
+      title: "청소하기",
+      completed: false,
+    },
+  ];
+
+  //** 클릭된 X id가 같은 데이터 지우는 함수 (filter)  */
+  handleClick = (id) => {
+    let newTodoData = this.todoData.filter((data) => data.id !== id);
+    console.log(newTodoData);
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="todoBlock">
+          <div className="title">
+            <h1>할일 목록</h1>
+          </div>
+
+          {this.todoData.map((data) => (
+            <div style={this.getStyle()} key={data.id}>
+              <input type="checkbox" defaultChecked={false} />
+              {data.title}
+              <button
+                style={this.btnStyle}
+                onClick={() => this.handleClick(data.id)}
+              >
+                X
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
