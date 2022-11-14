65aeeba5efee9d9808c3220f153f8b550dd46d2c
KIM JUN HYEOK <kjh45388117@gmail.com>
2022-11-14 18:01:51 +0900

    LoL

--- react-netflix/src/components/Nav.js
+++ react-netflix/src/components/Nav.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Nav.css";
 
 export default function Nav() {
   const [show, setShow] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
 
-  //   Useeffect 안에서 리스너를 하나 등록
+  //   UseEffect 안에서 리스너를 하나 등록
   // 컴포넌트 안쓸때 리스너를 등록한걸 없애줘야함
   // 그건 window.removeEventListener로 해준다.
   //**스크롤 할 때 작동 */
@@ -22,15 +25,29 @@ export default function Nav() {
     };
   }, []);
 
+  // 검색할 때 검색한 값과 같이 주소창이 번경됨
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  };
+
   return (
     //show가 true 일때 nav__black 활성화
     <nav className={`nav ${show && "nav__black"}`}>
       <img
-        alt="Netfllix logo"
+        alt="Netflix logo"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/200px-Netflix_2015_logo.svg.png"
         className="nav__logo"
         //리로드
         onClick={() => window.location.reload()}
+      />
+      {/* 검색창  */}
+      <input
+        value={searchValue}
+        onChange={handleChange}
+        className="nav__input"
+        type="text"
+        placeholder="Search Movie"
       />
 
       <img
